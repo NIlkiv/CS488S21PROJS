6d9dee57183a35c9bef9f32a5edf5b605069ed02
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-03-09 17:59:30 -0500

    Update Iperfer.py

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -4,13 +4,13 @@ import sys
 import pickle
 
 # Create server
-ServerName = input("Enter Hostname ")
+ServerName = sys.args()    #input("Enter Hostname ")
 if ServerName == 0 or ServerName == '':
     print("Error: missing or additional arguments")
     sys.exit()
 
 ServerIP = socket.gethostbyname(ServerName)
-ServerPort = input("Enter Port from range 1024 - 65535: ") #takes in port
+ServerPort = sys.args()    #input("Enter Port from range 1024 - 65535: ") #takes in port
 if ServerPort == '':
     print("Error: missing or additional arguments")
     sys.exit()
@@ -19,7 +19,7 @@ if int(ServerPort) < 1024 or int(ServerPort) > 65535:
     print(f"Port {ServerPort} out of range")
     sys.exit()
 
-sendTime = input("Enter time in seconds ")
+sendTime = sys.args()      #input("Enter time in seconds ")
 if sendTime == '':
     print("Error: missing or additional arguments")
     sys.exit()
@@ -46,19 +46,17 @@ while 1:
     clientSocket.send(data_string) #sends packets
 
     modified_sent = clientSocket.recvfrom(2048)  #receive return from server
-
-                                     
-                                     
+       
     byt = byt + len(modified_sent)   #start count of bytes sent by the length of Byt array
     end = time.time()                #get 2nd local time form machine to establish end time
-    elp = end - start                #total time elepssed
+    elp = end - start                #total time elapsed
 
-    if elp >= float(sendTime):       #if the elepssed time is greater than time usr entered end the count and break the loop
+    if elp >= float(sendTime):       #if the elapsed time is greater than time usr entered end the count and break the loop
         break
 
    # else:
    # continue
-#print(str("BYTES: " + str(byt) + "/" + str(elp) + "time")) #print total bytes sent + time elepssed
+#print(str("BYTES: " + str(byt) + "/" + str(elp) + "time")) #print total bytes sent + time elapsed
 
 bandwith = (int(byt)/int(elp))/1024                                              #calc bandwith
 print("Sent=" + str(byt) + " kB " + "rate="  + str(bandwith) + "Mbps")           #print bandwith
